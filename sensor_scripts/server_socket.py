4495d9799a8c7da8825fa956b05792e53cf40458
Carlo Andreotti <andreotti.carlo@gmail.com>
2016-08-18 14:23:01 +0200

    Receive and save sensor IP address too

--- sensor_scripts/server_socket.py
+++ sensor_scripts/server_socket.py
@@ -3,74 +3,101 @@ import sys
 from thread import *
 import time
 import json
+import os
+
+log_file = 'socket_logger.log'
+dir_path = os.path.dirname(os.path.abspath(__file__))
+f = open(os.path.join(dir_path, log_file), 'a')
 
 HOST = ''
 PORT = 3080
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+f.write('Socket created\n')
 print 'Socket created'
 
 try:
     s.bind((HOST, PORT))
 except socket.error as msg:
+    f.write("ERROR: Bind failed. Error Code: " + str(msg[0]) + ' Message ' + msg[1] + "\n")
     print 'Bind failed. Error Code: ' + str(msg[0]) + ' Message ' + msg[1]
+    f.close()
     sys.exit()
 
+f.write('Socket bind complete\n')
 print 'Socket bind complete'
 
 s.listen(10)
+f.write('Socket now listening on port ' + str(PORT) + '\n')
 print 'Socket now listening on port ' + str(PORT)
+f.close()
 
 def clientthread(conn):
-    #conn.send('xxx')
+    f = open(os.path.join(dir_path, log_file), 'a')
     total_data=[]
     while True:
         data = conn.recv(4096)
         if not data:
             break
         total_data.append(data)
-        #print 'Received: ' + data.decode('utf-8')
 
     conn.close()
     json_string = ''.join(total_data)
-    print 'Total: ' + json_string
-    sensor_data = json.loads(json_string)                       
-    print 'TempC' + str(sensor_data["tempC"])                              
-    degrees = sensor_data["tempC"]                                         
-    hectopascals = sensor_data["pressure"] / 100                           
-    altitude = sensor_data["altitudeM"]                                    
-    humidity = sensor_data["humidity"]                                     
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())       
-    print 'Timestamp = %s'%timestamp + ', Temp = {0:0.3f} deg C'.format(degrees) + ', Pressure = {0:0.2f} hPa'.format(hectopascals) + ', Humidity = {0:0.2f} %'.format(humidity)
+    f.write('Received: ' + json_string + '\n')
+    print 'Received: ' + json_string
+    sensor_data = json.loads(json_string)
+    sensor_ip = sensor_data["sensorIP"]
+    degrees = sensor_data["tempC"]
+    hectopascals = sensor_data["pressure"] / 100
+    #altitude = sensor_data["altitudeM"]
+    humidity = sensor_data["humidity"]
+    timestamp = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
 
     import sqlite3
-    import os
-    dir_path = os.path.dirname(os.path.abspath(__file__))
     try:
         db = sqlite3.connect(os.path.join(dir_path, '../raspi-weather.db'))
         c = db.cursor()
+        c.execute("""CREATE TABLE IF NOT EXISTS sensors(
+            'ip'            TEXT PRIMARY KEY,
+            'name'          TEXT,
+            'description'   TEXT,
+            'altitude'      NUMERIC)""")
         c.execute("""CREATE TABLE IF NOT EXISTS indoor(
             `id`            INTEGER PRIMARY KEY AUTOINCREMENT,
             `timestamp`     DATETIME,
             `temperature`   NUMERIC,
             `humidity`      NUMERIC,
-            `pressure`      NUMERIC)""")
+            `pressure`      NUMERIC,
+            'sensor_ip'     TEXT)""")
         db.commit()
-	args = [timestamp, round(degrees, 2), int(humidity), round(hectopascals, 2)]
-        c.execute('INSERT INTO indoor (timestamp, temperature, humidity, pressure) VALUES (?, ?, ?, ?)', args)
+        sensor_args = [sensor_ip, 0]
+        c.execute('INSERT OR IGNORE INTO sensors (ip, altitude) VALUES (?, ?)', sensor_args)
+        db.commit()
+        c.execute('SELECT altitude FROM sensors WHERE ip = ?', (sensor_ip,) )
+        row = c.fetchone()
+        # Retrieve sensor altitude
+        altitude = row[0]
+        # Adjust pressure to sea level
+        hectopascals = hectopascals*(1-(0.0065 * altitude)/(degrees + 0.0065 * altitude + 273.15))**(-5.257)
+        f.write('Inserting data: Timestamp = %s'%timestamp + ', Temp = {0:0.3f} deg C'.format(degrees) + ', Pressure = {0:0.2f} hPa'.format(hectopascals) + ', Humidity = {0:0.2f} %'.format(humidity) + '\n')
+        print 'Inserting data: Timestamp = %s'%timestamp + ', Temp = {0:0.3f} deg C'.format(degrees) + ', Pressure = {0:0.2f} hPa'.format(hectopascals) + ', Humidity = {0:0.2f} %'.format(humidity)
+        args = [timestamp, round(degrees, 2), int(humidity), round(hectopascals, 2), sensor_ip]
+        c.execute('INSERT INTO indoor (timestamp, temperature, humidity, pressure, sensor_ip) VALUES (?, ?, ?, ?, ?)', args)
         db.commit()
         db.close()
         print("Done")
     except sqlite3.Error as err:
-        f = open(os.path.join(dir_path, 'logger.log'), 'a')
-        print(str(err))
-        f.write(str(err))
+        print("ERROR: " + str(err))
+        f.write("ERROR: " + str(err))
         f.write('\n')
-        f.close()
+    f.close()
 
 while 1:
     conn, addr = s.accept()
+    f = open(os.path.join(dir_path, log_file), 'a')
+    f.write('Connected with ' + addr[0] + ':' + str(addr[1]) + '\n')
     print 'Connected with ' + addr[0] + ':' + str(addr[1])
+    f.close()
     start_new_thread(clientthread, (conn,))
 
 s.close()
